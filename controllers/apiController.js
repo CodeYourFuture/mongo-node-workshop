b1e5d61761b188167bc3c4ea0b04a51eb77ad7c4
khaled <khaled.kzy@gmail.com>
2017-08-26 16:21:29 +0100

    I have changed the apiController to read from mongoDB and not from the posts files

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,27 +2,61 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
 
 router.use(bodyParser.json());
+const ObjectID = require('mongodb').ObjectID;
 
-router.get('/posts', (req, res, next) => {
-    const filePath = __dirname + '/../data/posts.json';
-    const callbackFunction = function(error, file) {
-        if(error) {
-            return next(error);
-        }
-        // we call .toString() to turn the file buffer to a String
-        const fileData = file.toString();
-        // we use JSON.parse to get an object out the String
-        const postsJson = JSON.parse(fileData);
-        res.json(postsJson);
-    };
-    fs.readFile(filePath, callbackFunction);
+
+
+router.get('/students', (req, res) => {
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find();
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students);
+        });
+    });
 });
 
-router.post('/posts', (req, res) => {
-    console.log(req.body);
-    res.status(500).send('not implemented');
+router.get('/students/:id', (req, res) => {
+    const studentId = req.params.id;
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find(ObjectID(studentId));
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students);
+        });
+    });
 });
 
+router.get('/posts/:id', (req, res,next) => {
+    //const studentId = req.params.id;
+    const postId = req.params.id;
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find(ObjectID(postId));
+        cursor.toArray((error, posts) => {
+            db.close();
+            res.json(posts);
+        });
+    });
+});
+
+router.get('/posts/', (req, res,next) => {
+    
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find();
+        cursor.toArray((error, posts) => {
+            db.close();
+            res.json(posts);
+        });
+    });
+});
+
+
+    
 module.exports = router;
