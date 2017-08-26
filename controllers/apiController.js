72b80e6da62e2e6bcc7142844edd40323384bc51
Won Maung Thein <wonmaungthein@gmail.com>
2017-08-26 16:23:08 +0100

    using mongo

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,27 +2,57 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 router.use(bodyParser.json());
 
-router.get('/posts', (req, res, next) => {
-    const filePath = __dirname + '/../data/posts.json';
-    const callbackFunction = function(error, file) {
-        if(error) {
-            return next(error);
-        }
-        // we call .toString() to turn the file buffer to a String
-        const fileData = file.toString();
-        // we use JSON.parse to get an object out the String
-        const postsJson = JSON.parse(fileData);
-        res.json(postsJson);
-    };
-    fs.readFile(filePath, callbackFunction);
+router.get('/students/:studentid', (req, res) => {
+    const studentID = req.params.studentid;
+    console.log(studentID);
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find(ObjectID(studentID));
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students[0]);
+        });
+    });
 });
 
-router.post('/posts', (req, res) => {
-    console.log(req.body);
-    res.status(500).send('not implemented');
+router.get('/students/country/:country', (req, res) => {
+    const studentsdefinedbyCountry = req.params.country;
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find({"Country":studentsdefinedbyCountry});
+        cursor.toArray((error, studentsdefinedbyCountry) => {
+            db.close();
+
+            res.json(studentsdefinedbyCountry);
+        });
+
+    });
+
 });
 
-module.exports = router;
+
+        router.get('/posts', (req, res, next) => {
+            const mongoConnection = 'mongodb://localhost:27017/profile';
+            MongoClient.connect(mongoConnection, (err, db) => {
+                const cursor = db.collection('posts').find({});
+                cursor.toArray((error, posts) => {
+                    db.close();
+                    res.json(posts);
+                });
+            });
+
+        });
+
+        router.post('/posts', (req, res) => {
+            console.log(req.body);
+            res.status(500).send('not implemented');
+        });
+
+        module.exports = router;
