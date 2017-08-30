7dc3318d83d717f3f9018c2e5b165f23d26e56f0
panditita <topimorita@gmail.com>
2017-08-30 13:27:27 +0100

    Link to a single post

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const mongoConnection = "mongodb://127.0.0.1:27017/profile";
+
+//Change to E6
+
 
 
 router.get('/', function (req, res) {
-    const mongoConnection = process.env.MONGODB_URI|| 'mongodb://localhost:27017/profile';
-
     MongoClient.connect(mongoConnection, (err, db) => {
         const cursor = db.collection("posts").find({});
         cursor.toArray((error, posts) => {
@@ -15,12 +17,30 @@ router.get('/', function (req, res) {
             // send the data to the Template to render
             res.render('index', {
                 title: "Etza's profile",
-                subheading: "A modern Website built in Node with Handlebars",
+                subheading: "A modern site driven by a database",
                 posts: posts
             });
         });
     });
 });
+
+router.get('/post-:postId', function (req, res) {
+    const postId = req.params.postId;
+
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection("posts").find(ObjectID(postId));
+        cursor.toArray((error, posts) => {
+            db.close();
+            // send the data to the Template to render
+            res.render('single-view', {
+                title: posts[0].title,
+                subheading: "A modern site driven by a database",
+                post: posts[0]
+            });
+        });
+    });
+});
+
 
 
 router.get('/my-cv', function (req, res) {
