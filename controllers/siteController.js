c5bcbf50e4f91bd49cc211a578eb83708f4482ee
YJohn <yfekadu2016@gmail.com>
2017-08-30 23:08:13 +0100

    refactoring the function

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -2,39 +2,35 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const {objectID} = require('mongodb');
+const dbClient = require('../helpers/dbClient');
 const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/profile';
-const objectID = require('mongodb').objectID;
 
-router.get('/', function (req, res) {
-    // Write code to connect to database and return posts
-    console.log(mongoConnection);
-    MongoClient.connect(mongoConnection, (err, db) => {
-        const cursor = db.collection('posts').find({});
-        cursor.toArray((error, posts) => {
-            db.close();
-            // res.json(posts);
-            res.render('index', {
-                title: "Yohannes's profile",
-                subheading: "A Great Website driven by a database",
-                posts: posts
-            });
+router.get('/', (req, res) =>{
+    const callback = (error, posts) => {
+        res.render('index', {
+            title: "Yohannes's profile",
+            subheading: "A Great Website driven by a database",
+            posts: posts
         });
-    });
+    };
+    dbClient.getPosts({}, callback);
 });
 
 router.get('/post-:postId', (req, res) => {
     const postId = req.params.postId;
-    MongoClient.connect(mongoConnection, (err, db) => {
-        const cursor = db.collection('posts').find(ObjectID(postId));
-        cursor.toArray((error, posts) => {
-            db.close();
-            res.render('single-post', {
-                title: posts[0].title,
-                subheading: "A Great Website driven by a database",
-                post: posts[0]
-            });
+
+    const callback = (error, posts) => {
+
+        res.render('single-post', {
+            title: posts[0].title,
+            subheading: "A Great Website driven by a database",
+            post: posts[0]
         });
-    });
+    }
+    dbClient.getPosts({
+        _id: objectID(postId)
+    }, callback);
 });
 
 router.get('/my-cv', function (req, res) {
