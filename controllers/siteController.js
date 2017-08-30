4100c788cb3b0674cd4d5b130a32a29af5dc432d
YJohn <yfekadu2016@gmail.com>
2017-08-30 22:27:41 +0100

    added single-post view

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/profile';
-    
+const objectID = require('mongodb').objectID;
+
 router.get('/', function (req, res) {
     // Write code to connect to database and return posts
     console.log(mongoConnection);
@@ -13,14 +14,28 @@ router.get('/', function (req, res) {
             db.close();
             // res.json(posts);
             res.render('index', {
-                title: "Michael's profile",
-                subheading: "A modern Website built in Node with Handlebars",
+                title: "Yohannes's profile",
+                subheading: "A Great Website driven by a database",
                 posts: posts
             });
         });
     });
 });
 
+router.get('/post-:postId', (req, res) => {
+    const postId = req.params.postId;
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find(ObjectID(postId));
+        cursor.toArray((error, posts) => {
+            db.close();
+            res.render('single-post', {
+                title: posts[0].title,
+                subheading: "A Great Website driven by a database",
+                post: posts[0]
+            });
+        });
+    });
+});
 
 router.get('/my-cv', function (req, res) {
     res.render('my-cv');
