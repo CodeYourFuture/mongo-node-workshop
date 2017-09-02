9c53608b102f4e5420f9607e8423150886bfb165
Won Maung Thein <wonmaungthein@gmail.com>
2017-09-02 16:17:21 +0100

    Added Post.js model

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,6 +7,19 @@ const ObjectID = require('mongodb').ObjectID;
 
 router.use(bodyParser.json());
 
+
+router.get('/students', (req, res) => {
+
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find({});
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students);
+        });
+    });
+});
+
 router.get('/students/:studentid', (req, res) => {
     const studentID = req.params.studentid;
     console.log(studentID);
@@ -21,12 +34,13 @@ router.get('/students/:studentid', (req, res) => {
     });
 });
 
+
 router.get('/students/country/:country', (req, res) => {
     const studentsdefinedbyCountry = req.params.country;
     const mongoConnection = 'mongodb://localhost:27017/profile';
 
     MongoClient.connect(mongoConnection, (err, db) => {
-        const cursor = db.collection('students').find({"Country":studentsdefinedbyCountry});
+        const cursor = db.collection('students').find({ "Country": studentsdefinedbyCountry });
         cursor.toArray((error, studentsdefinedbyCountry) => {
             db.close();
 
@@ -37,22 +51,21 @@ router.get('/students/country/:country', (req, res) => {
 
 });
 
-
-        router.get('/posts', (req, res, next) => {
-            const mongoConnection = 'mongodb://localhost:27017/profile';
-            MongoClient.connect(mongoConnection, (err, db) => {
-                const cursor = db.collection('posts').find({});
-                cursor.toArray((error, posts) => {
-                    db.close();
-                    res.json(posts);
-                });
-            });
-
+router.get('/posts', (req, res, next) => {
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find({});
+        cursor.toArray((error, posts) => {
+            db.close();
+            res.json(posts);
         });
+    });
 
-        router.post('/posts', (req, res) => {
-            console.log(req.body);
-            res.status(500).send('not implemented');
-        });
+});
 
-        module.exports = router;
+router.post('/posts', (req, res) => {
+    console.log(req.body);
+    res.status(500).send('not implemented');
+});
+
+module.exports = router;
--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 const dbClient = require('../helpers/dbClient');
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const Post = require('../models/Posts');
+const formidable = require('express-formidable');
+
 const mongoConnection = process.env.MONGO_URI || 'mongodb://localhost:27017/profile';
+// const mongoose = require('mongoose');
 
 // router.get('/', function (req, res) { 'mongodb://localhost:27017/profile';
 
-router.get('/', (req, res)=> {
+router.use(formidable());
+router.get('/', (req, res) => {
     const callback = (error, posts) => {
         res.render('index', {
             title: "Won's profile",
@@ -16,24 +22,59 @@ router.get('/', (req, res)=> {
             posts: posts
         });
     }
-    dbClient.getPosts({}, callback);
+    mongoose.connect(mongoConnection);
+    Post.find({}, callback);
 });
+
+
+router.post('/save-post', (req, res) => {
+
+    const post = req.fields;
+
+    console.log(req.fields); // contains non-file fields
+    const callback = (error, post) => {
+        // handle any errors which might ocur
+        if (error) {
+            console.error(error);
+            return res.redirect('/error');
+        }
+
+        console.log('post saved successfully', post);
+        res.redirect('/');
+    }
+
+    mongoose.connect(mongoConnection);
+    // This is using the Model to create an object
+    // based on the fields submitted from the form
+    const newPost = new Post(req.fields);
+    newPost.save(callback);
+});
+
+
 
 
 router.get('/post-:postId', (req, res) => {
     const postId = req.params.postId;
 
     const callback = (error, posts) => {
-        res.render('single-post', {
-            title: post[0].title,
+        
+        res.render('single-view', {
+            title: posts[0].title,
             subheading: "A modern Website built in Node with Handlebars",
             post: posts[0]
         });
     };
 
     dbClient.getPosts({
-        _id: ObjectId(postId)},callback);
+        _id: ObjectId(postId)
+    }, callback);
 });
+
+
+
+
+
+
 
 
 // creating a new function 
