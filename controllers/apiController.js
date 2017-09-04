741f305fe86b9c85078e8ad9bc267489b79dab3d
Won Maung Thein <wonmaungthein@gmail.com>
2017-09-04 03:16:24 +0100

    I have to fix the problem for post

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
+const Post = require('../models/Posts');
 
 router.use(bodyParser.json());
-
 
 router.get('/students', (req, res) => {
 
@@ -43,7 +44,6 @@ router.get('/students/country/:country', (req, res) => {
         const cursor = db.collection('students').find({ "Country": studentsdefinedbyCountry });
         cursor.toArray((error, studentsdefinedbyCountry) => {
             db.close();
-
             res.json(studentsdefinedbyCountry);
         });
 
@@ -51,21 +51,43 @@ router.get('/students/country/:country', (req, res) => {
 
 });
 
-router.get('/posts', (req, res, next) => {
-    const mongoConnection = 'mongodb://localhost:27017/profile';
-    MongoClient.connect(mongoConnection, (err, db) => {
-        const cursor = db.collection('posts').find({});
-        cursor.toArray((error, posts) => {
-            db.close();
-            res.json(posts);
-        });
+router.get('/api/posts', (req, res, next) => {
+
+    const post = req.body;
+
+    console.log(req.body); // contains non-file fields
+    const callback = (error, post) => {
+        // handle any errors which might ocur
+        if (error) {
+            console.error(error);
+            return res.redirect('/error');
+        }
+
+        console.log('post saved successfully', post);
+        res.send(post);
+    }
+
+    mongoose.connect(mongoConnection);
+    // This is using the Model to create an object
+    // based on the fields submitted from the form
+    const newPost = new Post(req.body);
+    newPost.save(callback);
+
+    router.post('/posts', (req, res) => {
+        console.log(req.body);
+        res.status(500).send('not implemented');
     });
 
-});
 
-router.post('/posts', (req, res) => {
-    console.log(req.body);
-    res.status(500).send('not implemented');
-});
+    //     const mongoConnection = 'mongodb://localhost:27017/profile';
+    //     MongoClient.connect(mongoConnection, (err, db) => {
+    //         const cursor = db.collection('posts').find({});
+    //         cursor.toArray((error, posts) => {
+    //             db.close();
+    //             res.json(posts);
+    //         });
+    //     });
 
-module.exports = router;
+    // });
+
+    module.exports = router;
